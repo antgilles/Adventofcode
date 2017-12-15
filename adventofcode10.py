666995b954ae323ba71b1abcc019c9119aedd6bd
Antoine GILLES <gilleant@credit-agricole.net>
2017-12-15 14:39:46 +0100

    day10 v2

--- adventofcode10.py
+++ adventofcode10.py
@@ -1,24 +1,27 @@
-from itertools import cycle
 
-liste = [i for i in range(5)]
+liste = [i for i in range(256)]
 print(liste)
 
-liste=cycle(liste)
-
-
-length = [3, 4, 1, 5]
-
+length = [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110]
+#length = [3, 4, 1, 5]
 pos = 0
-skip = 1
+skip = 0
 
 for i in length:
-    print(liste)
-    posend = (pos + i )
-    print(pos)
-    print(posend)
-    rep = list(liste[pos:posend])
-    rep.reverse()
-    liste[pos:posend]=rep
+    #print(liste)
+    posend = (pos + i ) % len(liste)
+    print("pos: %s,  posend: %s, len: %s, skip :%s" % (pos, posend, i, skip))
 
-    pos = posend + skip
+    if posend <= pos:
+        rep = list(liste[pos:]) + list(liste[:posend])
+    else:
+        rep = list(liste[pos:posend])
+    rep.reverse()
+    for j in range(len(rep)):
+        liste[(pos + j) % len(liste)] = rep[j]
+    pos = (posend + skip) % len(liste)
     skip += 1
+
+print(liste)
+
+print(liste[0] * liste[1])
