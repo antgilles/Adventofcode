62b8b9e6761e597b97e66caec5d3a7a46b9569f3
Antoine GILLES <gilleant@credit-agricole.net>
2017-12-18 18:35:58 +0100

    13 part 2

--- adventofcode13.py
+++ adventofcode13.py
@@ -1,7 +1,7 @@
 import operator
 
 
-with open("advent13.txt") as f:
+with open("advent13big.txt") as f:
     lines = [i.strip() for i in f.readlines()]
 
 fw = {}
@@ -12,23 +12,21 @@ for line in lines:
         # FW [DEPTH, CUR, DIRECTION]
         fw[int(line[0])] = [int(line[1]),0,operator.add]
 
-print(fw)
+#print(fw)
 
 caught = []
 for iteration in range(max(fw.keys()) + 1 ):
     print ("===============iteration %s" % iteration )
     if iteration in fw and fw[iteration][1]==0:
-        print("caught")
-        caught.append([iteration, fw[iteration][0]])
+	print("caught")
+	caught.append([iteration, fw[iteration][0]])
     for level, values in fw.items():
-        values[1] = values[2](values[1],1)
-        if values[1] < 0:
-            values[1] = 0
-            values[2] = operator.add
-        elif  values[1] == values[0] - 1:
-            values[1] = values[0] - 1
-            values[2] = operator.sub
-        print(fw)
+	values[1] = values[2](values[1],1)
+	if values[1] == 0:
+	    values[2] = operator.add
+	elif  values[1] == values[0] - 1:
+	    values[2] = operator.sub
+	#print(fw)
     print(caught)
 result = 0
 l = [level * depth for level, depth in caught]
