640480818920384887dc7cfa9a6e8b7778395cfd
antgilles <antgilles@gmail.com>
2017-12-20 00:48:25 +0100

    functions in 10

--- adventofcode10-2.py
+++ adventofcode10-2.py
@@ -1,17 +1,8 @@
 import functools
 
-liste = [i for i in range(256)]
-#liste = [i for i in range(5)]
-print(liste)
-
-length = [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110]
-length = [3, 4, 1, 5]
-phrase = '225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110'
-
-length = [ord(i) for i in phrase] + [17, 31, 73, 47, 23]
-print(length)
-pos = 0
-skip = 0
+def getinput(s):
+    length = [ord(i) for i in phrase] + [17, 31, 73, 47, 23]
+    return length
 
 def getdensehash(l):
     ret = []
@@ -19,24 +10,30 @@ def getdensehash(l):
         ret.append(functools.reduce(lambda i, j: int(i) ^ int(j), l[(i * 16):(i * 16) + 16]))
     return ret
 
-for iteration in range(64):
-    for i in length:
-        #print(liste)
-        posend = (pos  + i )
-        #print("pos: %s,  posend: %s, len: %s, skip :%s" % (pos, posend, i, skip))
+def gethash(s):
+    l = getinput(s)
+    liste = [i for i in range(256)]
+    pos = 0
+    skip = 0
+    for iteration in range(64):
+        for i in l:
+            posend = (pos  + i )
+            if posend > len(liste):
+                posend = posend % len(liste)
+                rep = list(liste[pos:]) + list(liste[:posend])
+            else:
+                rep = list(liste[pos:posend])
+            rep.reverse()
+            for j in range(len(rep)):
+                liste[(pos % len(liste)+ j) % len(liste)] = rep[j]
+            pos = (pos + i + skip) % len(liste)
+            skip += 1
+    densehash = getdensehash(liste)
+    h = ''.join(['{:02x}'.format(i) for i in densehash])
+    return h
 
-        if posend > len(liste):
-            posend = posend % len(liste)
-            rep = list(liste[pos:]) + list(liste[:posend])
-        else:
-            rep = list(liste[pos:posend])
-        rep.reverse()
-        for j in range(len(rep)):
-            liste[(pos % len(liste)+ j) % len(liste)] = rep[j]
-        pos = (pos + i + skip) % len(liste)
-        skip += 1
+phrase = '225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110'
+phrase = '1,2,3'
+#phrase = 'flqrgnkx-0'
 
-print(liste)
-densehash = getdensehash(liste)
-h = ''.join(['{:02x}'.format(i) for i in densehash])
-print(h)
+print(gethash(phrase))
