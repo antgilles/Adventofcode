e87b5a353a3a899c872421f26758e1f951bcddb4
antgilles <antgilles@gmail.com>
2017-12-19 22:56:12 +0100

    working but how ?

--- adventofcode10.py
+++ adventofcode10.py
@@ -10,20 +10,20 @@ skip = 0
 
 for i in length:
     print(liste)
-    posend = (pos + i ) % len(liste)
+    posend = (pos % len(liste) + i )
     print("pos: %s,  posend: %s, len: %s, skip :%s" % (pos, posend, i, skip))
 
-    if posend <= pos:
-        rep = list(liste[pos:]) + list(liste[:posend])
+    if posend > len(liste):
+        rep = list(liste[pos % len(liste):]) + list(liste[0:posend % len(liste)])
     else:
-        rep = list(liste[pos:posend])
+        rep = list(liste[pos % len(liste):posend])
     print(rep)
     rep.reverse()
     print(rep)
     for j in range(len(rep)):
         print("replacing char %s par %s" %((pos +j) % len(liste), rep[j]))
-        liste[(pos + j) % len(liste)] = rep[j]
-    pos = (posend + skip) % len(liste)
+        liste[(pos % len(liste)+ j) % len(liste)] = rep[j]
+    pos = (pos + i + skip)
     skip += 1
     print(liste)
 
