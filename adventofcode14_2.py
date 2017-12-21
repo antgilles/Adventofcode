56f6ff7fd822ced1e0c2e9e2d8f28e7fb141da2b
antgilles <antgilles@gmail.com>
2017-12-21 01:39:17 +0100

    temp try

--- adventofcode14_2.py
+++ adventofcode14_2.py
@@ -1,7 +1,9 @@
 from  adventofcode10_2 import gethash
 
+nbgrp = 0
 
 def parseneighbour(x,y):
+    global nbgrp
     print('parsing: %s %s => %s' % (x,y, matr[x][y]))
     if matr[x][y] == 1:
         matr[x][y] = 2
@@ -15,14 +17,14 @@ def parseneighbour(x,y):
             parseneighbour(x,y - 1)
 
 def phrastomatrix(phrase):
-    matr = [[0 for i in range(128)] for j in range(128)]
+    ret = list()
     for y in range(128):
         h = gethash(phrase + '-' + str(y))
-        line = "{0:8b}".format(int(h,16))
-        for x in range(len(line)):
-            if line[x] == '1':
-                matr[x][y]= 1
-    return matr
+        print(h)
+        line = ''.join(["{:04b}".format(int(i,16)) for i in h]) 
+        print(len(line))
+        ret.append(line)
+    return ret
 
 
 
@@ -30,21 +32,33 @@ phrase = 'jzgqcdpd'
 phrase = 'flqrgnkx'
 
 matr = phrastomatrix(phrase)
-#matr = [[0,1,0],
-#        [1,0,1],
-#        [0,1,0]]
+#matr = [[1,1,0],
+#       [1,0,1],
+#      [0,1,1]]
+print(matr)
+print(matr[127][127])
 
-
-nbgrp = 0
-for x in range(128):
-    for y in range(128):
-        print('x = %s' % x)
-        print('y = %s' % y)
-        print('parsing OUT : %s %s => %s' % (x,y, matr[x][y]))
-        if matr[x][y] == 1:
+visited = set()
+moves = [(0,1),(0,-1),(1,0),(-1,0)]
+for y in range(len(matr)):
+    for x in range(len(matr)):
+        #print('parsing : %s %s => %s' % (x,y, matr[x][y]))
+        if matr[x][y] == "1" and (x,y) not in visited:
             nbgrp += 1
-            print('=====> UP')
-            parseneighbour(x,y)
+            visited.add((x,y))
+            q = [(x,y)]
+            while q:
+                x,y = q.pop()
+                for dx, dy in moves:
+                    nx , ny = x + dx, y + dy
+                    if 0 <= nx < len(matr) and  0 <= ny < len(matr) and matr[nx][ny] == "1" and (nx,ny) not in visited:
+                        #print('parsing IN: %s %s => %s' % (nx,ny, matr[nx][ny]))
+                        visited.add((nx,ny))
+                        q.append((nx,ny))
+
+
+
+
 
 #print(matr)
 
